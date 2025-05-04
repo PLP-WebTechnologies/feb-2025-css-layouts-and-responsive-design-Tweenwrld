a7c06015189d923f44604d5f156d9189b10931a7
Tweenwrld <154606522+Tweenwrld@users.noreply.github.com>
2025-05-04 22:17:29 +0300

    Created script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Mobile menu toggle functionality
+document.getElementById('menuToggle').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
